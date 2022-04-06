368f2c30148c354ae239f7595af7bf0a7197c9f2
hristo <hristopliokov@gmail.com>
2022-04-06 13:24:10 +0300

    Initial commit

---
+++ Exam2.1.js
@@ -0,0 +1,22 @@
+const sortingArray = (arr, fn) => {
+  const len = arr.length;
+
+  for (let i = 0; i < len; i++) {
+    for (let j = 0; j < len - i - 1; j++) {
+      if (!fn(arr[j], arr[j + 1])) {
+        const temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+};
+
+const array = [6, 4, 3, 1, 9, 44, 33, 2];
+const func = (left, right) => {
+  return left < right;
+};
+console.log(sortingArray(array, func));
+
+
