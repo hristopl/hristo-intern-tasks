4ea6eb45c3596a24effe93af34ca6a43ebc3fd0d
hristo <hristopliokov@gmail.com>
2022-04-12 23:09:41 +0300

    Deleted folders

--- chapter-2/Exam2.4-partTwo.js
+++
@@ -1,9 +0,0 @@
-const calculate = (arr, number) => {
-  return arr.reduce(
-    (result, curr) => (curr % number === 0 ? [...result, curr] : result),
-    []
-  );
-};
-const arr1 = [1, 2, 3, 4, 5, 6, 7];
-const divisible = 2;
-console.log(calculate(arr1, divisible));
